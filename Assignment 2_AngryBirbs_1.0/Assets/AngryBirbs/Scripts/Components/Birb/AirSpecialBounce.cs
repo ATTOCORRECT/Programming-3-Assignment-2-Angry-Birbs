66a81aa14bd65caa3cd1b95c0413c77d8cd489bf
Auto <97545662+ATTOCORRECT@users.noreply.github.com>
2023-11-06 11:35:55 -0500

    air special explode implemented

--- Assignment 2_AngryBirbs_1.0/Assets/AngryBirbs/Scripts/Components/Birb/AirSpecialBounce.cs
+++ Assignment 2_AngryBirbs_1.0/Assets/AngryBirbs/Scripts/Components/Birb/AirSpecialBounce.cs
@@ -9,7 +9,7 @@ public class AirSpecialBounce : MonoBehaviour, IAirSpecial
     {
         Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
         Vector2 velocity = rigidbody.velocity;
-        if (velocity.y >= 0) // velocity reflection when moving upward
+        if (velocity.y <= 0) // velocity reflection when moving upward
         {
             // new velocity is the velocity i want after the impulse
             Vector2 newVelocity = new Vector2(velocity.x, -velocity.y * SlowDownFactor); // slowfactor was changed to only influence vertical speed because killing the horizontal speed too feels bad to play with.
--- Assignment 2_AngryBirbs_1.0/Assets/AngryBirbs/Scripts/Components/Birb/AirSpecialExplode.cs
+++ Assignment 2_AngryBirbs_1.0/Assets/AngryBirbs/Scripts/Components/Birb/AirSpecialExplode.cs
@@ -1,12 +1,46 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class AirSpecialExplode : MonoBehaviour, IAirSpecial
 {
     [Range( 0, 5 )]
     public float BlastRadius = 2;
 
+    private List<Collider2D> targetColliders = new List<Collider2D>();
+
+
+    public void Start()
+    {
+        // collider setup
+        CircleCollider2D ExplosionCollider = gameObject.AddComponent<CircleCollider2D>();
+        ExplosionCollider.isTrigger = true;
+        ExplosionCollider.radius = BlastRadius;
+    }
+
     public void ExecuteAirSpecial()
     {
-        throw new System.NotImplementedException();
+        for ( int i = 0; i < targetColliders.Count; i++ ) // destroy all objects currently in the list
+        {
+            targetColliders[i].gameObject.GetComponent<Target>().DestroyTarget();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) // on enter
+    {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Target") // if its of the layer "Target"
+        {
+            if (!targetColliders.Contains(collision)) // and isnt already in the list
+            {
+                targetColliders.Add(collision); // add to list
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) // on exit 
+    {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Target") // if its of the layer "Target"
+        {
+            targetColliders.Remove(collision); // remove from list
+        }
     }
 }
